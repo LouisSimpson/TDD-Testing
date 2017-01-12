053c35f0528a079ec4bf3d774a049a20d4dfd5a6
LouisSimpson <louissimpson1996@gmail.com>
2017-01-12 22:37:38 +0000

    Initial upload

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStock
+    {
+        public clsStock()
+        {
+        }
+
+        public string Stock { get; set; }
+        public int stockLevel { get; set; }
+       
+
+        public bool valid(string stockName)
+        {
+            if ( stockName.Length == 0)
+            {
+                return false;
+            }
+            if (stockName.Length > 50)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+       
+
+        public bool valid2(int StockLevel)
+        {
+            if ( StockLevel == -1 )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ TestFramework/tstStock.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace TestFramework
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock TestStock = new clsStock();
+            Assert.IsNotNull(TestStock);
+        }
+
+        [TestMethod]
+        public void StockNameOK()
+        {
+            clsStock TestStock = new clsStock();
+            string TestData = "GTX 1080";
+            TestStock.Stock = TestData;
+            Assert.AreEqual(TestStock.Stock, TestData);
+        }
+
+        [TestMethod]
+        public void StockLevelOK()
+        {
+            clsStock TestStock = new clsStock();
+            int stocklevel = 1;
+            TestStock.stockLevel = stocklevel ;
+            Assert.AreEqual(TestStock.stockLevel, stocklevel);
+        }
+
+
+
+
+
+        [TestMethod]
+        public void ValidExists()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("GTX 1080");
+            Assert.IsTrue(OK);
+            
+        }
+
+        [TestMethod]
+        public void ValidExistsStocklevel()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid2(0);
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockLevelminlessone()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid2(0);
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockLevelmax()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid2(100);
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockLevelminminusone()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid2(-1);
+            Assert.IsFalse(OK);
+
+        }
+
+
+
+
+        [TestMethod]
+        public void StockNameMinLessOne()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("");
+            Assert.IsFalse(OK);
+
+        }
+
+        [TestMethod]
+        public void StockMinPlusOne()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("ab");
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockMin()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("a");
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockMaxMinusOne()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("ahsnfhthfnajskfnshdngrehwushtngfighipandshitpgiod");
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockMax()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("abjfndkfjgnfmgnfjgnfmgktmewignrjajsndjcnfdotpgiltl");
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockMaxPlusOne()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("ahsjtigjthnklawheithasjwuftownfhcjakleyhsnfujlajsn1");
+            Assert.IsFalse(OK);
+
+        }
+
+        [TestMethod]
+        public void StockMid()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            OK = TestStock.valid("ajajajajajajajajajajajaja");
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void StockExtremeMax()
+        {
+            clsStock TestStock = new clsStock();
+            Boolean OK;
+            string TestData = "";
+            TestData = TestData.PadRight(500, 'a');
+            OK = TestStock.valid(TestData);
+            Assert.IsFalse(OK);
+
+        }
+
+
+    }
+}
